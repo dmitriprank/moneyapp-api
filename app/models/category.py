34452ff1733ba2fcfe33bf594e937aacc5aead02
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-15 22:31:33 -0600

    Testing

--- app/models/category.py
+++ app/models/category.py
@@ -9,7 +9,9 @@ class CategoryModel(db.Model):
     __tablename__ = "category"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.Enum(TransactionType, name="category_type", create_type=True), nullable=False)
+    # type = db.Column(db.Enum(TransactionType, name="category_type", create_type=True), nullable=False)
+    type = db.Column(db.Enum(*[e.value for e in TransactionType], name="category_type", create_type=True),
+                     nullable=False)
     name = db.Column(db.String(64))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),  nullable=False)
 
