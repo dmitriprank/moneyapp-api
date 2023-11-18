e652938a1e1b0747c3a4b6794f786c0c792872b3
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-17 23:18:05 -0600

    Edited migration script

--- migrations/versions/3700130a79d3_.py
+++ migrations/versions/3700130a79d3_.py
@@ -9,6 +9,8 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql as pg
 
+from app.models.default_category import insert_default_categories
+
 
 # revision identifiers, used by Alembic.
 revision = '3700130a79d3'
@@ -55,6 +57,7 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    insert_default_categories()
     # ### end Alembic commands ###
 
 
