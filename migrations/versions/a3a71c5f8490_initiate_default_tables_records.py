977e550bc970b0cacafab99daecf0717969f8195
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-18 05:23:14 +0000

    Added new revision file

---
+++ migrations/versions/a3a71c5f8490_initiate_default_tables_records.py
@@ -0,0 +1,24 @@
+"""initiate_default_tables_records
+
+Revision ID: a3a71c5f8490
+Revises: 3700130a79d3
+Create Date: 2023-11-18 05:22:57.739476
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a3a71c5f8490'
+down_revision = '3700130a79d3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
