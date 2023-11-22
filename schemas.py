82d98f00e01b9a4a560245e2f7f66813c66eb446
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-22 16:06:23 -0600

    Added category_id to transaction response schema

--- schemas.py
+++ schemas.py
@@ -29,6 +29,7 @@ class PlainTransactionSchema(Schema):
     id = fields.Int(dump_only=True)
     type = fields.Enum(TransactionType, by_value=True, required=True)
     amount = fields.Str(required=True)
+    category = fields.Nested(PlainCategorySchema)
     category_id = fields.Integer(required=True)
     description = fields.String(validate=validate.Length(max=512))
     date = fields.Date(default=date.today())
