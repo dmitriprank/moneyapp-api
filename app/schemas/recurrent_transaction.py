b91e7970da52d4f045913a20469fbc0abf4aea09
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-23 13:18:35 -0600

    Added category field to RecurrentTransactionShema as nested from Category

--- app/schemas/recurrent_transaction.py
+++ app/schemas/recurrent_transaction.py
@@ -3,6 +3,7 @@ from marshmallow import fields
 
 from app.models import RecurrentTransactionModel
 from app.types import TransactionType, RecurrentFrequency
+from app.schemas.category import CategoryNestedSchema
 
 
 class RecurrentTransactionSchema(SQLAlchemyAutoSchema):
@@ -12,6 +13,8 @@ class RecurrentTransactionSchema(SQLAlchemyAutoSchema):
         exclude = ['user_id']
 
     id = auto_field(dump_only=True)
-    timestamp = auto_field(dump_only=True)
+    category_id = fields.Integer(required=True, load_only=True)
+    category = fields.Nested(CategoryNestedSchema, dump_only=True)
     type = fields.Enum(TransactionType, by_value=True, required=True)
     frequency = fields.Enum(RecurrentFrequency, by_value=True, required=True)
+    timestamp = auto_field(dump_only=True)
