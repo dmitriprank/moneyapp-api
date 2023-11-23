f41b5aaaf58e5e128c4d245199f70a036889d0f5
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-23 12:14:18 -0600

    Added include_fk = True param to Mata of the RecurrentTransactionSchema

--- app/schemas/recurrent_transaction.py
+++ app/schemas/recurrent_transaction.py
@@ -5,6 +5,7 @@ from app.models import RecurrentTransactionModel
 class RecurrentTransactionSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = RecurrentTransactionModel
+        include_fk = True
 
     id = auto_field(dump_only=True)
     timestamp = auto_field(dump_only=True)
