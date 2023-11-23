5a67def52646eb7914f1cb2c4873c18fa7924338
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-23 14:26:19 -0600

    Added next_transaction date change to cron script

--- recurrent_transactions_scripts/create_transactions.py
+++ recurrent_transactions_scripts/create_transactions.py
@@ -1,13 +1,15 @@
 from app.resources.transaction import create_transaction
 from app.models import RecurrentTransactionModel
+from db import db
 
-from datetime import datetime
+import datetime
 from typing import List
 
 
 def create_recurrent_transaction():
-    today_transactions: List[RecurrentTransactionModel] = RecurrentTransactionModel.query.filter_by(
-                                                            next_transaction=datetime.now().date()).all()
+    today_transactions: List[RecurrentTransactionModel] = (RecurrentTransactionModel.query.filter_by(
+                                                            next_transaction=datetime.datetime.now().date())
+                                                           .all())
     if not today_transactions:
         return
 
@@ -19,6 +21,9 @@ def create_recurrent_transaction():
             "description": r_t_data.description
         }
         create_transaction(transaction_data, user_id=r_t_data.user_id)
+        r_t_data.next_transaction = r_t_data.next_transaction + datetime.timedelta(days=30)
+        db.session.add(r_t_data)
+        db.session.commit()
 
 
 if __name__ == '__main__':
