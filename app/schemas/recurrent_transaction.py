d5b936b47977e3f4e317b14224ec2f5de54b5eda
dmitriprank <dmitriprank.telegram@gmail.com>
2023-11-23 12:20:31 -0600

    Exclueded user_id from RecurrentTransactionSchema schema

--- app/schemas/recurrent_transaction.py
+++ app/schemas/recurrent_transaction.py
@@ -6,6 +6,7 @@ class RecurrentTransactionSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = RecurrentTransactionModel
         include_fk = True
+        exclude = ['user_id']
 
     id = auto_field(dump_only=True)
     timestamp = auto_field(dump_only=True)
